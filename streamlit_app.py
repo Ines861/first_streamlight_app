142861a03ff7e36a1adf22cda4a786623d710d2e
Ines861 <125281591+Ines861@users.noreply.github.com>
2023-03-27 11:16:40 +0100

    Update streamlit_app.py
    Edited title
    Edited header
    Added item
    Inserted emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.title('My Mom's new healthy dinner')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')
+streamlit.text('🍞🥑 Avocado Toast'
+  
